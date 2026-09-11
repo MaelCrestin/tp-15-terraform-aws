7935efcc4815c8bd1d93c7fd1048ff846e3ac780
MaelCrestin <mael.crestin@gmail.com>
2026-09-11 14:52:03 +0200

    Ajout passerelle internet et routage public (bloc 2)

---
+++ public_routing.tf
@@ -0,0 +1,25 @@
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "tp-15-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "tp-15-rt-public"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
